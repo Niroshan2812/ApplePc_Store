1ab862820ef13ec26b705576c62aff280246c22f
Niroshan <niroshan9812@gmail.com>
2024-12-26 23:50:58 +0530

    Initial commit

---
+++ src/main/java/org/niroshan/applepc_store/ApplePcStoreApplication.java
@@ -0,0 +1,13 @@
+package org.niroshan.applepc_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplePcStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplePcStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/niroshan/applepc_store/ApplePcStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.niroshan.applepc_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplePcStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
