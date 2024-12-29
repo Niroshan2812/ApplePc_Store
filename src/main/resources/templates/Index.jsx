a1939313b473eccd749018a3cf20fdc80d6e627c
Niroshan <niroshan9812@gmail.com>
2024-12-29 21:43:56 +0530

    Data can save in DB

---
+++ src/main/resources/templates/Index.jsx
@@ -0,0 +1,11 @@
+ function exampleImpl(){
+    return (
+
+        <>
+        <div>
+            <p>This is an example Impl</p>
+        </div>
+        </>
+    )
+}
+export default exampleImpl();
